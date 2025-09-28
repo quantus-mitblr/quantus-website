d629e3585bd8f499f15ea52a3fe6295ce1419303
Aman Kumar Giri <amangiri.dev@gmail.com>
2025-09-28 23:03:07 +0530

    chore: added dist dir

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // since next/image optimization needs a server
   },
-  distDir: "build", // custom build directory
+  distDir: "dist", // custom build directory
   basePath: "/quantus-website", // repo name as base path
   assetPrefix: "/quantus-website/", 
 };
